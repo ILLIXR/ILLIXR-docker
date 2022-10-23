837deadb59cc039fdb5d586bdbbbcec8382b365c
shg8 <me@shg.life>
2022-10-23 15:55:51 -0500

    Optimize Dockerfile to reduce intermediate containers

--- src/create_user.sh
+++ src/create_user.sh
@@ -5,4 +5,5 @@ adduser --disabled-password --gecos '' ${USER}
 echo "${USER}:${PASSWORD}" | chpasswd
 echo 'PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin"' >> /home/${USER}/.profile
 
+mkdir -p /etc/sudoers.d/
 echo "${USER} ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers.d/nopasswd
